49a1e02eb5ce1b0a8c4511a6427d2f879224d8ad
kyrylsavin <43367700+kyrylsavin@users.noreply.github.com>
2019-04-25 08:50:09 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
-var button = document.getElementById("submit_button");
+//2
+var str="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, at";
+var arr = new Array();
 
-button.addEventListener("click", function(){
-	var data = new Object();
-	var input = document.getElementsByTagName("input");
-	
-	for (i in input) {
-		try {
-			if (input[i].getAttribute("type") == "radio" || 
-				input[i].getAttribute("type") == "checkbox" )
-				data[input[i].getAttribute("value")] = input[i].checked;
-			else {
-				data[input[i].getAttribute("name")] = input[i].value;
-			}
-		}
-		catch (e) {
-			break;
-		}
+for (i = 0; i < str.length; i++) {
+	if (str[i] == "a") {
+		arr.push(i);
 	}
-	console.log(data);
-});
+}
+console.log(arr);
+
+//1
+
+var seconds = 234245645335;
+
+var hours = Math.floor(seconds/3600);
+var minutes = Math.floor((seconds - hours*3600)/60);
+var seconds = (seconds - (hours*3600))%60;
+
+console.log(`${hours}:${minutes}:${seconds}`);
